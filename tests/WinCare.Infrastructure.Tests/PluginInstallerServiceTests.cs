67c69e1dad3bdff42d583db355061db27b57e8b1
AmirrezaFarnamTaheri <taheri.farnam@gmail.com>
2026-08-15 03:06:20 +0330

    fix(tests): add missing using System.Text namespace

--- tests/WinCare.Infrastructure.Tests/PluginInstallerServiceTests.cs
+++ tests/WinCare.Infrastructure.Tests/PluginInstallerServiceTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.IO.Compression;
 using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using WinCare.Infrastructure.Plugins;
