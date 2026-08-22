130b6bcd4ea13addd9d0827d7f18a39b2e598f2c
AmirrezaFarnamTaheri <taheri.farnam@gmail.com>
2026-08-22 23:21:43 +0330

    fix: disambiguate protocol activation args

--- src/WinCare.App/App.xaml.cs
+++ src/WinCare.App/App.xaml.cs
@@ -1,6 +1,6 @@
 using Microsoft.UI.Xaml;
 using Microsoft.Windows.AppLifecycle;
-using Windows.ApplicationModel.Activation;
+using ProtocolActivatedEventArgs = Windows.ApplicationModel.Activation.ProtocolActivatedEventArgs;
 using WinCare.Infrastructure.Observability;
 
 namespace WinCare.App;
