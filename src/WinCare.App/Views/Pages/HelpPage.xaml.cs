f2dbd0337f5fd2569747f6e371858cdd6ed69877
Farnam <taheri.farnam@gmail.com>
2026-09-05 19:07:21 +0330

    feat: wire Help guidance to product destinations

--- src/WinCare.App/Views/Pages/HelpPage.xaml.cs
+++ src/WinCare.App/Views/Pages/HelpPage.xaml.cs
@@ -1,6 +1,14 @@
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+
 namespace WinCare.App.Views.Pages;
+
 public sealed partial class HelpPage : Page
 {
     public HelpPage() => InitializeComponent();
+
+    private void OpenCheckup_Click(object sender, RoutedEventArgs e) => PageNavigation.NavigateTo(this, "checkup");
+    private void OpenActivity_Click(object sender, RoutedEventArgs e) => PageNavigation.NavigateTo(this, "activity");
+    private void OpenPluginStore_Click(object sender, RoutedEventArgs e) => PageNavigation.NavigateTo(this, "plugin-store");
+    private void OpenSettings_Click(object sender, RoutedEventArgs e) => PageNavigation.NavigateTo(this, "settings");
 }
